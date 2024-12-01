8eec39ca5130fd2f0000e76f7d048ba4da722d44
Hoang Dinh <dldh.work@gmail.com>
2024-12-01 14:44:48 +0700

    Add outer loop for url_id.py

--- url_id.py
+++ url_id.py
@@ -1,28 +1,31 @@
 import json
 
-profile = 0
-url_array = []
-
 while True:
-  profile = int(input("Profile No.: "))
-  if profile <= 0:
-    print("Invalid input.")
-  else:
-    break
+  profile = 0
+  url_array = []
 
-while True: 
-  count = int(input("Number of url: "))
-  if count <= 0:
-    print("Invalid input.")
-  else:
-    break
+  while True:
+    profile = int(input("Profile No.: "))
+    if profile <= 0:
+      print("Invalid input.")
+    else:
+      break
 
-for i in range(count):
-  id = input(f"Enter url id {i + 1}: ")
-  url = f"https://store.steampowered.com/join/completesignup?creationid={id}"
-  url_array.append(url)
+  while True: 
+    count = int(input("Number of url: "))
+    if count <= 0:
+      print("Invalid input.")
+    else:
+      break
 
-file_path = f"data/url/url-{profile}.json"
+  for i in range(count):
+    id = input(f"Enter url id {i + 1}: ")
+    url = f"https://store.steampowered.com/join/completesignup?creationid={id}"
+    url_array.append(url)
 
-with open(file_path, "w") as json_file:
-  json.dump(url_array, json_file)
+  file_path = f"data/url/url-{profile}.json"
+
+  with open(file_path, "w") as json_file:
+    json.dump(url_array, json_file)
+
+  print("")
